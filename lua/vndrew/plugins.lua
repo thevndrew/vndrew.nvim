795fffe86c953b8831a15e37f33138ab82731df6
andrew <69527486+thevndrew@users.noreply.github.com>
2024-05-11 13:30:34 -0400

    Try new import method for plugin config

--- lua/vndrew/plugins.lua
+++ lua/vndrew/plugins.lua
@@ -1,6 +1,6 @@
 -- [[ Configure plugins ]]
 local function configure(plugin)
-  require('vndrew/plugins/' .. plugin)
+  require('plugins/' .. plugin)
 end
 
 configure 'cmp'
@@ -20,12 +20,5 @@ configure 'which-key'
 
 -- configure 'neo-tree'
 -- configure 'tokyonight'
-
--- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---    This is the easiest way to modularize your config.
---
---  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
---    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
--- { import = 'custom.plugins' },
 
 -- vim: ts=2 sts=2 sw=2 et
