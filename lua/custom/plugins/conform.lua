d2c24110536e6c2fbe08d2c1a69fa76a82b16d4a
andrew <69527486+thevndrew@users.noreply.github.com>
2024-10-19 21:05:14 -0400

    Update python formatter

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -29,7 +29,18 @@ return {
                 end,
                 formatters_by_ft = {
                     lua = { 'stylua' },
-                    python = { 'isort', 'black' },
+                    python = function(bufnr)
+                        if require('conform').get_formatter_info('ruff_format', bufnr).available then
+                            return { 'ruff_format' }
+                        else
+                            return { 'isort', 'black' }
+                        end
+                    end,
+                    -- Use the "*" filetype to run formatters on all filetypes.
+                    ['*'] = { 'codespell' },
+                    -- Use the "_" filetype to run formatters on filetypes that don't
+                    -- have other formatters configured.
+                    ['_'] = { 'trim_whitespace' },
                 },
             }
 
